b467d06b32d3a7d79ad577e0d5e9a47a74191aa4
Vitorscau <82234102+Vitorscau@users.noreply.github.com>
2024-05-02 21:10:44 -0300

    Rotação, Zom, Location do dojo

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Test : MonoBehaviour
+public class SceneController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +15,10 @@ public class Test : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void loadScene()
+    {
+        SceneManager.LoadScene("MainScene");
     }
 }
